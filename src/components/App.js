be27e1958d432af678d992e9be5ff88fd7c67288
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2023-06-15 19:39:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,33 @@
+// import React from 'react';
+
+// function App() {
+
+//   return (
+//     <div id='main'>
+      
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './redux/actions';
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
   return (
     <div id='main'>
-      
+      <h1>Counter App</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <span data-testid='counter'>{counter}</span>
+      <button onClick={() => dispatch(decrement())}>-</button>
     </div>
   );
 }
 
 export default App;
+
