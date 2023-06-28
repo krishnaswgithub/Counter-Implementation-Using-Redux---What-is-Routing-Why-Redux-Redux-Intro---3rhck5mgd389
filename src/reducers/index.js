231df23fdb5411d4c950a6440f69e82b65511969
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2023-06-28 14:15:40 +0530

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,22 +1,17 @@
-import { INCREMENT, DECREMENT } from './actions';
-
 const initialState = {
-  counter: 0
-};
-
-const counterReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case INCREMENT:
-      return {
-        counter: state.counter + 1
-      };
-    case DECREMENT:
-      return {
-        counter: state.counter - 1
-      };
-    default:
-      return state;
-  }
-};
-
-export default counterReducer;
+    counter: 0
+}
+const reducer = (state=initialState, action)=>{
+    switch(action.type){
+        case 'INCREMENT':
+            return {
+                counter: state.counter+1
+            }
+        case 'DECREMENT':
+            return {
+                counter: state.counter-1
+            }
+        default : return {counter:state.counter}
+    }
+}
+export default reducer;
